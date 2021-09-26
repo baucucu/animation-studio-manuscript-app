284c7b97140e7e48919a820d579cebca9254a9af
baucucu <alexandru.raduca@gmail.com>
2021-09-26 11:34:40 +0300

    manuscript ui update

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,24 +7,25 @@ const HomePage = () => (
     {/* <BlockTitle>Manuscript</BlockTitle> */}
     <Block >
       <Row>
-        <Col width="35">
+        <Col width="25">
           <List noHairlines>
             <ListItem>Client name</ListItem>
-            <ListItem>Project name <Chip>Project status</Chip></ListItem>
+            <ListItem>Project name</ListItem>
+            <ListItem><Chip>Project status</Chip></ListItem>
             <ListItem>Manuscript writer</ListItem>
             <ListItem></ListItem>
             
           </List>
         </Col>
-        <Col width="15">
+        <Col width="20">
           <List noHairlines>
             <ListItem>Language</ListItem>
-            <ListItem>Word count</ListItem>  
+            <ListItem>Target video length:</ListItem>
           </List>  
         </Col>
-        <Col width="15">
+        <Col width="20">
           <List noHairlines>
-            <ListItem>Target video length:</ListItem>
+            <ListItem>Word count</ListItem>  
             <ListItem>Number of scenes</ListItem>  
           </List> 
         </Col>
@@ -34,10 +35,10 @@ const HomePage = () => (
               <p slot="header">Time left to respond <Chip>00:00:00:00</Chip></p>
               <p slot="content">Number of revisions <Chip >2/5</Chip></p>
               
-              <Block slot="footer" style={{display: 'flex', flexDirection:'row', justifyContent:'space-between'}}>
-                <Button small iconSize="small" iconF7="alarm_fill" outline color="blue" style={{marginRight:"16px"}}>Ask for revision</Button>
+              <CardFooter>
                 <Button small iconSize="small" iconF7="checkmark_alt" raised fill color="green">Approve manuscript</Button>
-              </Block>
+                <Button small color="blue" style={{marginRight:"16px"}}>Ask for revision</Button>
+              </CardFooter>
             </Card>
           </Block>
           
@@ -64,10 +65,12 @@ const HomePage = () => (
             <p>
               Quisque eget vestibulum nulla. 
             </p>
+            <Button small outline color="purple" fill raised iconF7="speaker_1_fill" iconSize="small">Listen to AI Voiceover</Button>
           </CardContent>
           <CardFooter >
             <Button small fill raised color="green" iconF7="checkmark_alt" iconSize="small" style={{marginRight: '8px'}}>Approve</Button>
-            <Button small outline color="blue" iconF7="bubble_left_bubble_right" iconSize="small">Comment</Button>
+            
+            <Button small  color="blue">Comment</Button>
           </CardFooter>
         </Card>
       </SwiperSlide>))}
