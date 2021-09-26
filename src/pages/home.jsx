4499aa0320c01b39eaea8f87fa9685976bb67460
baucucu <alexandru.raduca@gmail.com>
2021-09-26 13:54:39 +0300

    manuscript ui update

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,61 +3,83 @@ import { Page, Navbar, Block,Chip, CardHeader,CardFooter, Gauge, SwiperSlide,Swi
 
 const HomePage = () => (
   <Page>
-    <Block style={{marginBottom:0}}>
+    <Block style={{marginBottom:0}}> 
       <Row>
-        <Col width="40">
-          <List noHairlines style={{margin:0}}>
-            <ListItem>Client name</ListItem>
-            <ListItem>Project name<Chip color="blue">Project status</Chip></ListItem>
-            <ListItem>Manuscript writer<Chip color="blue">Thomas Oddegaard</Chip></ListItem>
-            <ListItem></ListItem>
+        <Col width="40" >
+          <Card>
+            <List noHairlines style={{margin:0}} slot="content">
+              <ListItem>Client name</ListItem>
+              <ListItem>Project name<Chip color="blue">Project status</Chip></ListItem>
+              <ListItem>Manuscript writer<Chip color="blue">Thomas Oddegaard</Chip></ListItem>
+            </List>
+          </Card>
+
+        </Col>
+        <Col width="30">
+          <Card>
+            <Row slot="content">
+              <Col>
+                <List noHairlines style={{margin:0}}>
+                  <ListItem>Language<Chip color="blue">Swedish</Chip></ListItem>
+                  <ListItem>Target length:<Chip color="blue">60s</Chip></ListItem>
+                </List>  
+              </Col>
+              <Col>
+                <List noHairlines style={{margin:0}}>
+                  <ListItem>Word count<Chip color="blue">354</Chip></ListItem>  
+                  <ListItem>Scenes<Chip color="blue">8</Chip></ListItem>  
+                </List> 
+              </Col>
+            </Row>
             
-          </List>
-        </Col>
-        <Col width="15">
-          <List noHairlines style={{margin:0}}>
-            <ListItem>Language<Chip color="blue">Swedish</Chip></ListItem>
-            <ListItem>Target video length:<Chip color="blue">60s</Chip></ListItem>
-          </List>  
-        </Col>
-        <Col width="15">
-          <List noHairlines style={{margin:0}}>
-            <ListItem>Word count<Chip color="blue">354</Chip></ListItem>  
-            <ListItem>Number of scenes<Chip color="blue">6</Chip></ListItem>  
-          </List> 
+            <Button  slot="footer" iconF7="speaker_1_fill" small iconSize="small" color="purple" fill raised>Listen to AI Voiceover</Button>
+          </Card>
+          
         </Col>
         <Col width="30">
           <Block style={{margin:0}}>
             <Card className="elevation-4">
               
-              <CardContent>
-                <Row style={{display: 'flex'}}>
+              <CardHeader style={{alignSelf: "center"}}>Time left to respond: <Chip color="blue">00:00:00:00</Chip> </CardHeader>
+              <CardContent style={{paddingBottom:0}}>
+                <Row style={{display: 'flex', marginBottom:0}} >
                   <Col style={{display:"flex", flexDirection:"column",justifyContent: 'center'}}>
                     <Gauge
-                      size={140}
+                      size={128}
                       
                       type="semicircle"
-                      value={2/5}
-                      valueText="2 revisions"
+                      value={2/8}
+                      valueText="2/8"
                       valueTextColor="blue"
-                      valueFontSize={16}
+                      valueFontSize={10}
                       borderColor="blue"
-                      labelText="of 5 total"
+                      labelText="scenes"
                       labelTextColor="#333"
                       labelFontWeight={400}
                       labelFontSize={12 }
                       borderWidth={10}
                     />
                   </Col>
-                  <Col style={{display:"flex", flexDirection:"column", alignItems: "center"}}>
-                    <p slot="header">Time left to respond </p>
-                    <Chip color="blue">00:00:00:00</Chip>
+                  <Col style={{display:"flex", flexDirection:"column",justifyContent: 'center'}}>
+                    <Gauge
+                      size={128}
+                      
+                      type="semicircle"
+                      value={2/5}
+                      valueText="2/5"
+                      valueTextColor="blue"
+                      valueFontSize={10}
+                      borderColor="blue"
+                      labelText="revisions"
+                      labelTextColor="#333"
+                      labelFontWeight={400}
+                      labelFontSize={12 }
+                      borderWidth={10}
+                    />
                   </Col>
                 </Row>
-                
-                
               </CardContent>
-              
+
               <CardFooter>
                 <Button small iconSize="small" iconF7="checkmark_alt" raised fill color="green">Approve manuscript</Button>
                 <Button small color="blue" style={{marginRight:"16px"}}>Ask for revision</Button>
@@ -70,7 +92,7 @@ const HomePage = () => (
     </Block>
     <Block style={{display:'flex', flexDirection: 'row', margin:0}}>
       <Swiper pagination navigation speed={500} slidesPerView={3.33} spaceBetween={20}>
-      {[1,2,3,4,5,6].map( (card,id) => (
+      {[1,2,3,4,5,6,7,8].map( (card,id) => (
         <SwiperSlide>
           <Card key={id} className="demo-card-header-pic" className="elevation-4">
             <CardHeader
@@ -88,7 +110,7 @@ const HomePage = () => (
               <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
               </p>
-              <Button small outline color="purple" fill raised iconF7="speaker_1_fill" iconSize="small">Listen to AI Voiceover</Button>
+              
             </CardContent>
             <CardFooter >
               <Button small fill raised color="green" iconF7="checkmark_alt" iconSize="small" style={{marginRight: '8px'}}>Approve</Button>
