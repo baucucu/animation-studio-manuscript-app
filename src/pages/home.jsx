303c1788eb9ae9dbc15170b037a3909f822ba49d
baucucu <alexandru.raduca@gmail.com>
2021-09-26 10:19:26 +0300

    update

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
-import { Page, Navbar, Block, BlockTitle } from 'framework7-react';
+import { Page, Navbar, Block, BlockTitle, Row, Col, List, ListItem, Card, Button } from 'framework7-react';
 
 const HomePage = () => (
   <Page>
     <Navbar title="Manuscript" />
-    <BlockTitle>Manuscript</BlockTitle>
+    {/* <BlockTitle>Manuscript</BlockTitle> */}
     <Block strong>
-      <p>Fugiat perspiciatis excepturi, soluta quod non ullam deleniti. Nobis sint nemo consequuntur, fugiat. Eius perferendis animi autem incidunt vel quod tenetur nostrum, voluptate omnis quasi quidem illum consequuntur, a, quisquam.</p>
-      <p>Laudantium neque magnam vitae nemo quam commodi, in cum dolore obcaecati laborum, excepturi harum, optio qui, consequuntur? Obcaecati dolor sequi nesciunt culpa quia perspiciatis, reiciendis ex debitis, ut tenetur alias.</p>
-    </Block>
-    <Block strong>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni molestiae laudantium dignissimos est nobis delectus nemo ea alias voluptatum architecto, amet similique, saepe iste consectetur in repellat ut minus quibusdam!</p>
-      <p>Molestias et distinctio porro nesciunt ratione similique, magni doloribus, rerum nobis, aliquam quae reiciendis quasi modi. Nam a recusandae, fugiat in ea voluptates fuga eius, velit corrupti reprehenderit dignissimos consequatur!</p>
-      <p>Blanditiis, cumque quo adipisci. Molestiae, dolores dolorum quos doloremque ipsa ullam eligendi commodi deserunt doloribus inventore magni? Ea mollitia veniam nostrum nihil, iusto doloribus a at! Ea molestiae ullam delectus!</p>
+      <Row>
+        <Col width="33">
+          <List>
+            <ListItem>Client name</ListItem>
+            <ListItem>Project name</ListItem>
+            <ListItem>Manuscript writer</ListItem>
+          </List>
+        </Col>
+        <Col width="33">
+          <List>
+            <ListItem>Target video length:</ListItem>
+            <ListItem>Language</ListItem>
+            <ListItem>Word count</ListItem>
+            <ListItem>Number of scenes</ListItem>
+            <ListItem>Target video length</ListItem>
+          </List>
+        </Col><Col width="33">
+          <Block>
+            <Card>
+              <BlockTitle slot="header">Time left to respond <span slot="content">00:00:00:00</span></BlockTitle>
+              
+              <span slot="footer" style={{display: 'flex', flexDirection:'row', justifyContent:'space-between'}}>
+                <Button iconF7="alarm_fill" outline color="blue" style={{marginRight:"16px"}}>Ask for revision</Button>
+                <Button iconF7="checkmark_alt" raised fill color="green">Approve manuscript</Button>
+              </span>
+            </Card>
+            <Card></Card>
+          </Block>
+        </Col>
+      </Row>
     </Block>
   </Page>
 );
