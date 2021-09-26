62fdcdc8ad39ef5738ccb6cf4c82fdf520ae6fdf
baucucu <alexandru.raduca@gmail.com>
2021-09-26 10:26:26 +0300

    update

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Page, Navbar, Block, BlockTitle, Row, Col, List, ListItem, Card, Button } from 'framework7-react';
+import { Page, Navbar, Block,Chip, BlockTitle, Row, Col, List, ListItem, Card, Button } from 'framework7-react';
 
 const HomePage = () => (
   <Page>
@@ -11,21 +11,23 @@ const HomePage = () => (
           <List>
             <ListItem>Client name</ListItem>
             <ListItem>Project name</ListItem>
-            <ListItem>Manuscript writer</ListItem>
+            <ListItem><Chip>Project status</Chip></ListItem>
+            
           </List>
         </Col>
         <Col width="33">
           <List>
-            <ListItem>Target video length:</ListItem>
+            <ListItem>Manuscript writer</ListItem>
             <ListItem>Language</ListItem>
-            <ListItem>Word count</ListItem>
+            <ListItem>Target video length:</ListItem>
             <ListItem>Number of scenes</ListItem>
-            <ListItem>Target video length</ListItem>
+            <ListItem>Word count</ListItem>
           </List>
         </Col><Col width="33">
           <Block>
             <Card>
-              <BlockTitle slot="header">Time left to respond <span slot="content">00:00:00:00</span></BlockTitle>
+              <BlockTitle slot="header">Time left to respond <Chip>00:00:00:00</Chip></BlockTitle>
+              <BlockTitle slot="content">Number of revisions <Chip >2/5</Chip></BlockTitle>
               
               <span slot="footer" style={{display: 'flex', flexDirection:'row', justifyContent:'space-between'}}>
                 <Button iconF7="alarm_fill" outline color="blue" style={{marginRight:"16px"}}>Ask for revision</Button>
