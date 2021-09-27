7395f298f33c3a8bc12700588b9812483849f4ae
baucucu <alexandru.raduca@gmail.com>
2021-09-27 13:23:09 +0300

    manuscript ui update

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,18 +1,18 @@
 import React, {useEffect, useState} from 'react';
-import { Page, Navbar, Block,Chip, CardHeader,CardFooter, Gauge, SwiperSlide,Swiper,BlockTitle,CardContent,Link, Row, Col, List, ListItem, Card, Button } from 'framework7-react';
+import { Page, Badge, Icon, Block,Chip, CardHeader,CardFooter, Gauge, SwiperSlide,Swiper,BlockTitle,CardContent,Link, Row, Col, List, ListItem, Card, Button } from 'framework7-react';
 
 const HomePage = ({f7route, f7router}) => {
 
   return (
-    <Page>
+    <Page >
       <Block style={{marginBottom:0}}> 
         <Row>
           <Col width="40" >
             <Card className="elevation-4" style={{height:180}}>
               <List noHairlines style={{margin:0}} slot="content">
-                <ListItem>Client<Chip color="blue" outline>Client Name</Chip></ListItem>
-                <ListItem>Project<Chip color="blue">Project status</Chip><Chip color="blue" outline>{f7route.query.card}</Chip></ListItem>
-                <ListItem>Manuscript writer<Chip color="blue" outline>Thomas Oddegaard</Chip></ListItem>
+                <ListItem key="client">Client<Chip color="blue" outline>Client Name</Chip></ListItem>
+                <ListItem key="project">Project<Chip color="blue">Project status</Chip><Chip color="blue" outline>{f7route.query.card}</Chip></ListItem>
+                <ListItem key="manuscriptWriter">Manuscript writer<Chip color="blue" outline>Thomas Oddegaard</Chip></ListItem>
               </List>
             </Card>
 
@@ -22,19 +22,24 @@ const HomePage = ({f7route, f7router}) => {
               <Row slot="content">
                 <Col>
                   <List noHairlines style={{margin:0}}>
-                    <ListItem>Language<Chip color="blue">Swedish</Chip></ListItem>
-                    <ListItem>Target length:<Chip color="blue">60s</Chip></ListItem>
+                    <ListItem key="language">Language<Chip color="blue">Swedish</Chip></ListItem>
+                    <ListItem key="targetLength">Target length:<Chip color="blue">60s</Chip></ListItem>
                   </List>  
                 </Col>
                 <Col>
                   <List noHairlines style={{margin:0}}>
-                    <ListItem>Word count<Chip color="blue">354</Chip></ListItem>  
-                    <ListItem>Scenes<Chip color="blue">8</Chip></ListItem>  
+                    <ListItem key="wordCount">Word count<Chip color="blue">354</Chip></ListItem>  
+                    <ListItem key="scenes">Scenes<Chip color="blue">8</Chip></ListItem>  
                   </List> 
                 </Col>
               </Row>
-              
-              <Button  slot="footer" iconF7="speaker_1_fill" small iconSize="small" color="purple" fill raised>Listen to AI Voiceover</Button>
+              <CardFooter >
+                <div style={{display: "flex", flexDirection: "row", justifyContent: "flex-start"}}>
+                  <Button  slot="footer" iconF7="speaker_1_fill" small iconSize="small" color="purple" fill raised>Listen to AI Voiceover</Button>
+                  <Badge color="white" tooltip="Tooltip text"><Icon size="medium"  color="black" f7="info_circle_fill"></Icon></Badge>
+                </div>
+                <Button fill raised color="purple" iconF7="cloud_download" small iconSize="small">Download PDF</Button>
+              </CardFooter>
             </Card>
             
           </Col>
@@ -42,10 +47,14 @@ const HomePage = ({f7route, f7router}) => {
             <Block style={{margin:0}}>
               <Card className="elevation-4" style={{height:180}}>
                 
-                <CardHeader style={{alignSelf: "center"}}>Time left to respond: <Chip color="blue">00:00:00:00</Chip> </CardHeader>
+                <CardHeader style={{alignSelf: "center"}}>
+                  Time left to respond: 
+                  <Chip color="blue">00:00:00:00</Chip> 
+                  <Button iconF7=""  small>Extend time</Button>
+                </CardHeader>
                 <CardContent style={{paddingBottom:0}}>
                   <Row style={{display: 'flex', marginBottom:0}} >
-                    <Col style={{display:"flex", flexDirection:"column",justifyContent: 'center'}}>
+                    {/* <Col style={{display:"flex", flexDirection:"column",justifyContent: 'center'}}>
                       <Gauge
                         size={130}
                         
@@ -61,7 +70,7 @@ const HomePage = ({f7route, f7router}) => {
                         // labelFontSize={12 }
                         borderWidth={10}
                       />
-                    </Col>
+                    </Col> */}
                     <Col style={{display:"flex", flexDirection:"column",justifyContent: 'center'}}>
                       <Gauge
                         size={130}
@@ -82,9 +91,9 @@ const HomePage = ({f7route, f7router}) => {
                   </Row>
                 </CardContent>
 
-                <CardFooter>
-                  <Button small iconSize="small" iconF7="checkmark_alt" raised fill color="green">Approve manuscript</Button>
-                  <Button small color="blue" style={{marginRight:"16px"}}>Ask for revision</Button>
+                <CardFooter >
+                    <Button small iconSize="small" iconF7="checkmark_alt" raised fill color="green">Approve manuscript</Button>
+                    <Button small color="blue" style={{marginRight:"16px"}}>Ask for revision</Button>
                 </CardFooter>
               </Card>
             </Block>
@@ -95,8 +104,8 @@ const HomePage = ({f7route, f7router}) => {
       <Block style={{display:'flex', flexDirection: 'row', margin:0}}>
         <Swiper  navigation speed={500} slidesPerView={3.33} spaceBetween={20}>
         {[1,2,3,4,5,6,7,8].map( (card,id) => (
-          <SwiperSlide>
-            <Card key={id} className="demo-card-header-pic" className="elevation-4">
+          <SwiperSlide key={id}>
+            <Card  className="demo-card-header-pic" className="elevation-4">
               <CardHeader
                 className="no-border"
                 valign="bottom"
@@ -108,14 +117,30 @@ const HomePage = ({f7route, f7router}) => {
                 {/* Journey To Mountains */}
               </CardHeader>
               <CardContent>
-                <p className="date">Posted on January 21, 2015</p>
+                <h2 className="scene">Scene #</h2>
                 <p>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
                 </p>
                 
               </CardContent>
+              <CardContent>
+                <Icon f7="circle_grid_3x3"></Icon>
+                Section name
+              </CardContent>
+              <CardContent>
+                <Icon f7="circle_grid_3x3"></Icon>
+                Section name
+              </CardContent>
+              <CardContent>
+                <Icon f7="circle_grid_3x3"></Icon>
+                Section name
+              </CardContent>
+              <CardContent>
+                <Icon f7="circle_grid_3x3"></Icon>
+                Section name
+              </CardContent>
               <CardFooter >
-                <Button small fill raised color="green" iconF7="checkmark_alt" iconSize="small" style={{marginRight: '8px'}}>Approve</Button>
+                {/* <Button small fill raised color="green" iconF7="checkmark_alt" iconSize="small" style={{marginRight: '8px'}}>Approve</Button> */}
                 <Button small  color="blue">Comment</Button>
               </CardFooter>
             </Card>
